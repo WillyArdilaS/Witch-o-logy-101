eefd5ca9ab0f415d00a8069efc9b7cb14c215e97
William Ardila <williamardilasanchez@gmail.com>
2025-09-25 23:37:47 -0500

    feat(audio): Add audio manager
    Add music and sfx.
    Create an audio controller that is managed from the inspector.
    Reproduce music based on current scene.

--- Assets/Scripts/Global/GlobalGameManager.cs
+++ Assets/Scripts/Global/GlobalGameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GlobalGameManager : MonoBehaviour
 {
@@ -6,9 +7,11 @@ public class GlobalGameManager : MonoBehaviour
     public static GlobalGameManager instance;
 
     // === Managers ===
+    private AudioManager audioManager;
     private SceneSwitchManager sceneSwitchManager;
 
     // === Properties ===
+    public AudioManager AudioManager => audioManager;
     public SceneSwitchManager SceneSwitchManager => sceneSwitchManager;
 
     void Awake()
@@ -24,10 +27,28 @@ public class GlobalGameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+
     }
 
     private void InitializeManagers()
     {
+        if (audioManager == null) audioManager = GetComponentInChildren<AudioManager>();
         if (sceneSwitchManager == null) sceneSwitchManager = GetComponentInChildren<SceneSwitchManager>();
+
+        InitializeGameSettings();
+    }
+
+    private void InitializeGameSettings()
+    {
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "MainMenu":
+                audioManager.PlayMusic(AudioManager.MusicType.MainMenu, 0, audioManager.MainMenuSongVol);
+                break;
+            case "GameScene":
+                audioManager.PlayMusic(AudioManager.MusicType.InGame, 0, audioManager.GameSongVol);
+                break;
+        }
     }
 }
--- Assets/Scripts/Global/SceneSwitchManager.cs
+++ Assets/Scripts/Global/SceneSwitchManager.cs
@@ -5,6 +5,19 @@ public class SceneSwitchManager : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
+        switch (sceneName)
+        {
+            case "MainMenu":
+                GlobalGameManager.instance.AudioManager.PlayMusic(AudioManager.MusicType.MainMenu, 0, GlobalGameManager.instance.AudioManager.MainMenuSongVol);
+                break;
+            case "GameScene":
+                GlobalGameManager.instance.AudioManager.PlayMusic(AudioManager.MusicType.InGame, 0, GlobalGameManager.instance.AudioManager.GameSongVol);
+                break;
+            case "GameOver":
+                GlobalGameManager.instance.AudioManager.StopMusic();
+                break;
+        }
+
         SceneManager.LoadScene(sceneName);
     }
 }
